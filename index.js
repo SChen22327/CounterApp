a2a1762bad3a0d7c3c6d70d53a530e926514dc54
unknown <BT_3W2_04@BKTECH.LOCAL>
2024-10-15 10:39:41 -0400

    Finished Part A and B

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 //Counter App
+let count = 0
+function increment() {
+    count++
+    document.getElementById("count-el").innerHTML = count
+    console.log(count)
+}
 document.getElementById("count-el").innerText = 5
